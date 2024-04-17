b7a5448d8d1edbcba30826f760ffc52113ab7711
sabazios <sabaziose@gmail.com>
2024-04-17 12:10:20 +0300

    lazy events

--- vikhola.js
+++ vikhola.js
@@ -4,7 +4,7 @@ const server = new Server()
 
 server.on("kernel.request", () => new Promise(resolve => setImmediate(resolve)));
 // server.on("kernel.request", (event) => event.target.on('kernel.controller', _ => console.log('fire')));
-// server.on('kernel.error', error => console.log(error.error))
+server.on('kernel.error', error => console.log(error.error))
 // server.route('GET', '/', (request, response) => response.send({ message: "Hello World!" }));
 server.route('GET', '/', (ctx) => ctx.response.send({ message: "Hello World!" }));
 
